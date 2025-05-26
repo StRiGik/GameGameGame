c2e912f118441f13dcc9acd79b90b7fbd0f7e9cd
StRiGik <vovastriga2003@gmail.com>
2025-05-26 18:16:06 +0300

    поврот персонажа по направлению движения по оси Х, анимация передвижения, разделения модели персонажа на физическую и визуальную

--- Assets/Scripts/Character_scripts/PlayerMovement.cs
+++ Assets/Scripts/Character_scripts/PlayerMovement.cs
@@ -3,24 +3,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Настройки")]
-    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _moveSpeed = 150f;   
 
+    private Animator _anim;
     private Rigidbody2D _rb;
     private Vector2 _moveInput;
+    private bool _isRight = true;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        Moving();
+        if (_moveInput.x < 0 && _isRight)
+            Flip();
+        else if (_moveInput.x > 0 && !_isRight)
+            Flip();
+    }
+
+    private void Moving()
+    {
         _moveInput.x = Input.GetAxisRaw("Horizontal");
         _moveInput.y = Input.GetAxisRaw("Vertical");
+        _anim.SetFloat("moveX", _moveInput.x);
+        _anim.SetFloat("moveY", _moveInput.y);
     }
 
     private void FixedUpdate()
     {
-        _rb.velocity = _moveInput.normalized * _moveSpeed;
+        _rb.velocity = _moveInput.normalized * _moveSpeed * Time.deltaTime;
+    }
+
+    void Flip()
+    {
+        _isRight = !_isRight;
+        Vector2 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
